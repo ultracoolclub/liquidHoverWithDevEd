eaffcc47e2fab9dff786674377783ade8e5feaf0
ultracoolclub <jeremiah.ashcraft@gmail.com>
2020-03-25 23:31:54 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+  parent: document.querySelector('.distortion'),
+  intensity: 0.2,
+  image1: "./img/bw1.jpg",
+  image2: "./img/bw2.jpg",
+  angle: Math.PI / 2,
+  displacementImage: "./img/stripe1.png"
+});
